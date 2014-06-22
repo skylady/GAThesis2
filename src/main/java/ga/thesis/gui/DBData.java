8aaef23e16d31d6f799962378c9168d538f6cdcb
skylady.mary <skylady.mary@gmail.com>
2014-06-22 20:43:10 +0300

    Crud

--- src/main/java/ga/thesis/gui/DBData.java
+++ src/main/java/ga/thesis/gui/DBData.java
@@ -5,6 +5,7 @@
 package ga.thesis.gui;
 
 import javax.swing.*;
+import com.jgoodies.forms.factories.*;
 import com.jgoodies.forms.layout.*;
 
 /**
@@ -18,6 +19,9 @@ public class DBData extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Marianna Pasichnyk
+        comboBox1 = new JComboBox();
+        scrollPane2 = new JScrollPane();
+        table1 = new JTable();
 
         //======== this ========
 
@@ -29,12 +33,22 @@ public class DBData extends JPanel {
                 java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
         setLayout(new FormLayout(
-            "default, $lcgap, default",
-            "2*(default, $lgap), default"));
+            "5*(default, $lcgap), default",
+            "4*(default, $lgap), default"));
+        add(comboBox1, CC.xy(9, 3));
+
+        //======== scrollPane2 ========
+        {
+            scrollPane2.setViewportView(table1);
+        }
+        add(scrollPane2, CC.xy(9, 9));
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Marianna Pasichnyk
+    private JComboBox comboBox1;
+    private JScrollPane scrollPane2;
+    private JTable table1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
